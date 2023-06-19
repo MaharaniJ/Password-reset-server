7943ff172b13d325b8551f1c7d9d5690570d7e71
MaharaniJ <maharani.jeyaraman@outlook.com>
2023-06-19 16:25:21 +0530

    vercel config

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
     origin: "http://localhost:3000"
+    // https://legendary-platypus-57e55d.netlify.app/
 }));
 app.use(router);
 
